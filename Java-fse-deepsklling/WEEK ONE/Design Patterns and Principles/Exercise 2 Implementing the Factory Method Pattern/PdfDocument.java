7de16e970718c8febf581b2086e8308404525aa5
ABHIRAMA H H <abhiramahh02@gmail.com>
2025-07-02 08:44:39 +0530

    Patterns week 1 completed

---
+++ Java-fse-deepsklling/WEEK ONE/Design Patterns and Principles/Exercise 2 Implementing the Factory Method Pattern/PdfDocument.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class PdfDocument extends Document{
+	@Override
+	public void open() {
+        System.out.println("Opening a PDF document.");
+    }
+}
---
+++ Java-fse-deepsklling/WEEK ONE/Design Patterns and Principles/Exercise 2 Implementing the Factory Method Pattern/WordDocument.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class WordDocument extends Document{
+	@Override
+	public void open() {
+        System.out.println("Opening a Word document.");
+    }
+}
